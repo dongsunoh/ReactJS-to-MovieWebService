c1f0625f34601c03c0214872796cb524abc0e2e3
dsoh <dongsun.oh@gmail.com>
2025-07-23 16:37:18 +0900

    React.useEffect function 사용 - 두번째 파라미터 작성 시 특정 state가 변경 될때 마다 실행 되도록 할 수 있음. - useEffect(function, [state1, stat2]);

--- react-for-firstproject/src/App.js
+++ react-for-firstproject/src/App.js
@@ -3,15 +3,35 @@ import {useState, useEffect} from "react";
 function App() {
 
     const [counter, setCounter] = useState(0);
-    const onClick = () => {setCounter((current) => current + 1)}
+    const [keyword, setKeyword] = useState("");
 
-    console.log("i run all the time");
+    const onClick = () => {setCounter((current) => current + 1)}
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    };
+
     // 처음 rendering 될때 1번만 실행.
     useEffect(()=>{
-        console.log("CALL THE API...");
+        console.log("i run only one");
     }, []);
+
+    // 처음 rendering 될때 keyword state가 변화할 때 코드를 실행.
+    useEffect(() => {
+        console.log("i run when 'keyword' changes.");
+    }, [keyword]);
+
+    // 처음 rendering 될때 counter state가 변화할 때 코드를 실행.
+    useEffect(() => {
+        console.log("i run when 'counter' changes.");
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("i run when 'counter' and 'keyword' changes.");
+    }, [counter, keyword])
+
     return (
           <div>
+              <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
               <h1>{counter}</h1>
               <button onClick={onClick}>Click me</button>
           </div>
