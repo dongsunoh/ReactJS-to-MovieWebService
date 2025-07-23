232a8782392b8477e778cff120df8b72f81a50c7
dsoh <dongsun.oh@gmail.com>
2025-07-23 14:46:30 +0900

    React.useEffect function 사용 - 첫번째 파라미터에 function을 넣으므로써 처음 렌더링 될때 한번만 호출하고 그 다음 렌더링 될때는 호출되지 않음.

--- react-for-firstproject/src/App.js
+++ react-for-firstproject/src/App.js
@@ -1,12 +1,15 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
 
     const [counter, setCounter] = useState(0);
     const onClick = () => {setCounter((current) => current + 1)}
 
-    console.log("call on api");
-
+    console.log("i run all the time");
+    // 처음 rendering 될때 1번만 실행.
+    useEffect(()=>{
+        console.log("CALL THE API...");
+    }, []);
     return (
           <div>
               <h1>{counter}</h1>
