2d5f5ca5013f2f890c0e3af4e2c61a9950ba116e
dsoh <dongsun.oh@gmail.com>
2025-07-25 17:35:25 +0900

    React Router  - react-router-dom 설치 (npm install react-router-dom)  - Router를 활용하여 페이지 이동 기능을 처리 함. (route 별로)  - 7.7.1 버전은 react-router만 사용해도 무방한 것으로 보임.

--- react-for-firstproject/src/App.js
+++ react-for-firstproject/src/App.js
@@ -1,6 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    return null;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" Component={Home} />
+                <Route path="/movie/" Component={Detail} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
